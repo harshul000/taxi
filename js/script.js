9d10026c0f3a9aa7b6b403dd7545e12e1607e6ab
harshul000 <149097828+harshul000@users.noreply.github.com>
2025-01-20 12:04:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ $(document).ready(function(){
 
 
 });
+
+let menubar = document.querySelector('#menu-bars')
+let mynav = document.querySelector('.navbar');
+
+menubar.onclick = () =>{
+    menubar.classList.toggle('fa-times');
+    mynav.classList.toggle('active');
+}
